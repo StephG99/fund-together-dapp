57b22e6685319fe9bbc0dd9ee109233d3894925c
Stephen Gudijanto <stephen.gudijanto@gmail.com>
2025-02-14 11:55:32 +0800

    Updated ABIS

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,10 +1,403 @@
 // src/config/constants.js
 
-export const FACTORY_CONTRACT_ADDRESS = "0xbfeb6cbFff39d21A3eD510f5bBAfe3560431766D"; // Replace with your deployed address
+export const FACTORY_CONTRACT_ADDRESS =
+  "0xbfeb6cbFff39d21A3eD510f5bBAfe3560431766D"; // Replace with your deployed address
 export const FACTORY_CONTRACT_ABI = [
-    {"type":"function","name":"campaigns","inputs":[{"name":"","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"address","internalType":"contract CrowdfundingCampaign"}],"stateMutability":"view"},{"type":"function","name":"createCampaign","inputs":[{"name":"_name","type":"string","internalType":"string"},{"name":"_goal","type":"uint256","internalType":"uint256"},{"name":"_startAt","type":"uint32","internalType":"uint32"},{"name":"_endAt","type":"uint32","internalType":"uint32"}],"outputs":[],"stateMutability":"nonpayable"},{"type":"function","name":"getCampaignAddress","inputs":[{"name":"index","type":"uint256","internalType":"uint256"}],"outputs":[{"name":"","type":"address","internalType":"address"}],"stateMutability":"view"},{"type":"function","name":"getTotalCampaigns","inputs":[],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"isCampaign","inputs":[{"name":"","type":"address","internalType":"address"}],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"view"},{"type":"function","name":"validateCampaign","inputs":[{"name":"campaignAddress","type":"address","internalType":"address"}],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"view"},{"type":"event","name":"CampaignCreated","inputs":[{"name":"campaignAddress","type":"address","indexed":true,"internalType":"address"},{"name":"name","type":"string","indexed":false,"internalType":"string"},{"name":"goal","type":"uint256","indexed":false,"internalType":"uint256"},{"name":"startAt","type":"uint32","indexed":false,"internalType":"uint32"},{"name":"endAt","type":"uint32","indexed":false,"internalType":"uint32"},{"name":"owner","type":"address","indexed":false,"internalType":"address"}],"anonymous":false}
+  { type: "constructor", inputs: [], stateMutability: "nonpayable" },
+  {
+    type: "function",
+    name: "campaigns",
+    inputs: [{ name: "", type: "uint256", internalType: "uint256" }],
+    outputs: [
+      { name: "campaignAddress", type: "address", internalType: "address" },
+      { name: "owner", type: "address", internalType: "address" },
+      { name: "name", type: "string", internalType: "string" },
+      { name: "creationTime", type: "uint256", internalType: "uint256" },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "createCampaign",
+    inputs: [
+      { name: "_name", type: "string", internalType: "string" },
+      { name: "_goal", type: "uint256", internalType: "uint256" },
+      { name: "_durationInDays", type: "uint32", internalType: "uint32" },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "getAllCampaigns",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "tuple[]",
+        internalType: "struct CrowdfundingFactory.Campaign[]",
+        components: [
+          { name: "campaignAddress", type: "address", internalType: "address" },
+          { name: "owner", type: "address", internalType: "address" },
+          { name: "name", type: "string", internalType: "string" },
+          { name: "creationTime", type: "uint256", internalType: "uint256" },
+        ],
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "getUserCampaigns",
+    inputs: [{ name: "_user", type: "address", internalType: "address" }],
+    outputs: [
+      {
+        name: "",
+        type: "tuple[]",
+        internalType: "struct CrowdfundingFactory.Campaign[]",
+        components: [
+          { name: "campaignAddress", type: "address", internalType: "address" },
+          { name: "owner", type: "address", internalType: "address" },
+          { name: "name", type: "string", internalType: "string" },
+          { name: "creationTime", type: "uint256", internalType: "uint256" },
+        ],
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "owner",
+    inputs: [],
+    outputs: [{ name: "", type: "address", internalType: "address" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "paused",
+    inputs: [],
+    outputs: [{ name: "", type: "bool", internalType: "bool" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "togglePause",
+    inputs: [],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "userCampaigns",
+    inputs: [
+      { name: "", type: "address", internalType: "address" },
+      { name: "", type: "uint256", internalType: "uint256" },
+    ],
+    outputs: [
+      { name: "campaignAddress", type: "address", internalType: "address" },
+      { name: "owner", type: "address", internalType: "address" },
+      { name: "name", type: "string", internalType: "string" },
+      { name: "creationTime", type: "uint256", internalType: "uint256" },
+    ],
+    stateMutability: "view",
+  },
 ];
 
 export const CAMPAIGN_CONTRACT_ABI = [
-    {"type":"constructor","inputs":[{"name":"_name","type":"string","internalType":"string"},{"name":"_goal","type":"uint256","internalType":"uint256"},{"name":"_startAt","type":"uint32","internalType":"uint32"},{"name":"_endAt","type":"uint32","internalType":"uint32"},{"name":"_owner","type":"address","internalType":"address"}],"stateMutability":"nonpayable"},{"type":"function","name":"campaignEnded","inputs":[],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"view"},{"type":"function","name":"contribute","inputs":[],"outputs":[],"stateMutability":"payable"},{"type":"function","name":"contributions","inputs":[{"name":"","type":"address","internalType":"address"}],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"finalizeCampaign","inputs":[],"outputs":[],"stateMutability":"nonpayable"},{"type":"function","name":"getContribution","inputs":[{"name":"contributor","type":"address","internalType":"address"}],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"getEndAt","inputs":[],"outputs":[{"name":"","type":"uint32","internalType":"uint32"}],"stateMutability":"view"},{"type":"function","name":"getGoal","inputs":[],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"getName","inputs":[],"outputs":[{"name":"","type":"string","internalType":"string"}],"stateMutability":"view"},{"type":"function","name":"getStartAt","inputs":[],"outputs":[{"name":"","type":"uint32","internalType":"uint32"}],"stateMutability":"view"},{"type":"function","name":"getTimeRemaining","inputs":[],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"getTotalFundsRaised","inputs":[],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"function","name":"hasReachedGoal","inputs":[],"outputs":[{"name":"","type":"bool","internalType":"bool"}],"stateMutability":"view"},{"type":"function","name":"name","inputs":[],"outputs":[{"name":"","type":"string","internalType":"string"}],"stateMutability":"view"},{"type":"function","name":"owner","inputs":[],"outputs":[{"name":"","type":"address","internalType":"address"}],"stateMutability":"view"},{"type":"function","name":"refund","inputs":[],"outputs":[],"stateMutability":"nonpayable"},{"type":"function","name":"totalFundsRaised","inputs":[],"outputs":[{"name":"","type":"uint256","internalType":"uint256"}],"stateMutability":"view"},{"type":"event","name":"CampaignFailed","inputs":[{"name":"totalFundsRaised","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false},{"type":"event","name":"CampaignSucceeded","inputs":[{"name":"totalFundsRaised","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false},{"type":"event","name":"ContributionReceived","inputs":[{"name":"contributor","type":"address","indexed":true,"internalType":"address"},{"name":"amount","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false},{"type":"event","name":"FundsWithdrawn","inputs":[{"name":"owner","type":"address","indexed":true,"internalType":"address"},{"name":"amount","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false},{"type":"event","name":"RefundIssued","inputs":[{"name":"contributor","type":"address","indexed":true,"internalType":"address"},{"name":"amount","type":"uint256","indexed":false,"internalType":"uint256"}],"anonymous":false}
+  {
+    type: "constructor",
+    inputs: [
+      { name: "_owner", type: "address", internalType: "address" },
+      { name: "_name", type: "string", internalType: "string" },
+      { name: "_goal", type: "uint256", internalType: "uint256" },
+      { name: "_durationInDays", type: "uint256", internalType: "uint256" },
+    ],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "addTier",
+    inputs: [
+      { name: "_name", type: "string", internalType: "string" },
+      { name: "_amount", type: "uint256", internalType: "uint256" },
+    ],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "backers",
+    inputs: [{ name: "", type: "address", internalType: "address" }],
+    outputs: [
+      { name: "totalContribution", type: "uint256", internalType: "uint256" },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "contribute",
+    inputs: [{ name: "_tierIndex", type: "uint256", internalType: "uint256" }],
+    outputs: [],
+    stateMutability: "payable",
+  },
+  {
+    type: "function",
+    name: "deadline",
+    inputs: [],
+    outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "extendDeadline",
+    inputs: [{ name: "_daysToAdd", type: "uint256", internalType: "uint256" }],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "getCampaignStatus",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "uint8",
+        internalType: "enum CrowdfundingCampaign.CampaignState",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "getContractBalance",
+    inputs: [],
+    outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "getTiers",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "tuple[]",
+        internalType: "struct CrowdfundingCampaign.Tier[]",
+        components: [
+          { name: "name", type: "string", internalType: "string" },
+          { name: "amount", type: "uint256", internalType: "uint256" },
+          { name: "backers", type: "uint256", internalType: "uint256" },
+        ],
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "goal",
+    inputs: [],
+    outputs: [{ name: "", type: "uint256", internalType: "uint256" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "hasContributedTier",
+    inputs: [
+      { name: "_backer", type: "address", internalType: "address" },
+      { name: "_tierIndex", type: "uint256", internalType: "uint256" },
+    ],
+    outputs: [{ name: "", type: "bool", internalType: "bool" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "name",
+    inputs: [],
+    outputs: [{ name: "", type: "string", internalType: "string" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "owner",
+    inputs: [],
+    outputs: [{ name: "", type: "address", internalType: "address" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "paused",
+    inputs: [],
+    outputs: [{ name: "", type: "bool", internalType: "bool" }],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "refund",
+    inputs: [],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "removeTier",
+    inputs: [{ name: "_index", type: "uint256", internalType: "uint256" }],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "state",
+    inputs: [],
+    outputs: [
+      {
+        name: "",
+        type: "uint8",
+        internalType: "enum CrowdfundingCampaign.CampaignState",
+      },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "tiers",
+    inputs: [{ name: "", type: "uint256", internalType: "uint256" }],
+    outputs: [
+      { name: "name", type: "string", internalType: "string" },
+      { name: "amount", type: "uint256", internalType: "uint256" },
+      { name: "backers", type: "uint256", internalType: "uint256" },
+    ],
+    stateMutability: "view",
+  },
+  {
+    type: "function",
+    name: "togglePause",
+    inputs: [],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  {
+    type: "function",
+    name: "withdraw",
+    inputs: [],
+    outputs: [],
+    stateMutability: "nonpayable",
+  },
+  { type: "event", name: "CampaignPaused", inputs: [], anonymous: false },
+  {
+    type: "event",
+    name: "CampaignStateChanged",
+    inputs: [
+      {
+        name: "newState",
+        type: "uint8",
+        indexed: false,
+        internalType: "enum CrowdfundingCampaign.CampaignState",
+      },
+    ],
+    anonymous: false,
+  },
+  { type: "event", name: "CampaignUnpaused", inputs: [], anonymous: false },
+  {
+    type: "event",
+    name: "ContributionReceived",
+    inputs: [
+      {
+        name: "contributor",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "amount",
+        type: "uint256",
+        indexed: false,
+        internalType: "uint256",
+      },
+      {
+        name: "tierIndex",
+        type: "uint256",
+        indexed: false,
+        internalType: "uint256",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "DeadlineExtended",
+    inputs: [
+      {
+        name: "newDeadline",
+        type: "uint256",
+        indexed: false,
+        internalType: "uint256",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "RefundIssued",
+    inputs: [
+      {
+        name: "recipient",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "amount",
+        type: "uint256",
+        indexed: false,
+        internalType: "uint256",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "TierAdded",
+    inputs: [
+      { name: "name", type: "string", indexed: false, internalType: "string" },
+      {
+        name: "amount",
+        type: "uint256",
+        indexed: false,
+        internalType: "uint256",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "TierRemoved",
+    inputs: [
+      {
+        name: "index",
+        type: "uint256",
+        indexed: false,
+        internalType: "uint256",
+      },
+    ],
+    anonymous: false,
+  },
+  {
+    type: "event",
+    name: "Withdrawal",
+    inputs: [
+      {
+        name: "owner",
+        type: "address",
+        indexed: true,
+        internalType: "address",
+      },
+      {
+        name: "amount",
+        type: "uint256",
+        indexed: false,
+        internalType: "uint256",
+      },
+    ],
+    anonymous: false,
+  },
 ];
